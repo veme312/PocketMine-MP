72cf76b1851303304c57147a245523a26074ce10
Dylan K. Taylor <odigiman@gmail.com>
2018-05-08 15:34:44 +0100

    Protocol changes for 1.5.0.1

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -39,15 +39,15 @@ interface ProtocolInfo{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	public const CURRENT_PROTOCOL = 261;
+	public const CURRENT_PROTOCOL = 271;
 	/**
 	 * Current Minecraft PE version reported by the server. This is usually the earliest currently supported version.
 	 */
-	public const MINECRAFT_VERSION = 'v1.4.0';
+	public const MINECRAFT_VERSION = 'v1.5.0.1 beta';
 	/**
 	 * Version number sent to clients in ping responses.
 	 */
-	public const MINECRAFT_VERSION_NETWORK = '1.4.0';
+	public const MINECRAFT_VERSION_NETWORK = '1.5.0.1';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
