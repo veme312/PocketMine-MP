30dea854083df72286ce2592ded000fed04885e6
Dylan K. Taylor <odigiman@gmail.com>
2018-07-10 17:50:02 +0100

    fix versions

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -37,7 +37,7 @@ namespace pocketmine {
 	use pocketmine\wizard\SetupWizard;
 
 	const NAME = "PocketMine-MP";
-	const BASE_VERSION = "3.0.6";
+	const BASE_VERSION = "3.0.7";
 	const IS_DEVELOPMENT_BUILD = false;
 	const BUILD_NUMBER = 0;
 
